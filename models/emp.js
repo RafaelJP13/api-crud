a61b9b5d3a0cc817d493462800103f8bc5c5e72d
Rafael Fernandes <Rafaelsf013@gmail.com>
2021-04-23 14:08:25 -0300

    Espaçamento

--- models/emp.js
+++ models/emp.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose')
+const Schema   = mongoose.Schema
+const ObjectId = Schema.ObjectId
 
-const empSchema = new mongoose.Schema({
+const empSchema = new Schema({
+
+    empId:{
+
+        type:ObjectId,
+
+    },
 
     empName:{
 
