29effb598b10282bf6d802a07034b16445045ca6
Rafael Fernandes <Rafaelsf013@gmail.com>
2021-04-22 23:39:11 -0300

    Configuraçoes iniciais

---
+++ index.js
@@ -0,0 +1,10 @@
+const express  = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+app.get('/', (req, res) => res.send('Welcome'))
+
+mongoose.connect('mongodb://localhost/teste', {useNewUrlParser:true})
+mongoose.connection.once('open', () => console.log('Database connected successfully!')).on('error', err => console.log(err))
+
+app.listen(8000, () => console.log('Server Running...'))
