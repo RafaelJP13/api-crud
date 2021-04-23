d74d31d19951d687b31f95e8a2d7cbf47d80379a
Rafael Fernandes <Rafaelsf013@gmail.com>
2021-04-23 14:06:56 -0300

    Adicionar ES6

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const express  = require('express')
-const mongoose = require('mongoose')
+import express from 'express'
+import mongoose from 'mongoose'
+
 const app = express()
+
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true,}));
 
 const routes = require('./routes/emp')
 
@@ -8,7 +14,26 @@ app.use('/emp', routes)
 
 app.get('/', (req, res) => res.send('Welcome'))
 
-mongoose.connect('mongodb://localhost/teste', {useNewUrlParser:true})
-mongoose.connection.once('open', () => console.log('Database connected successfully!')).on('error', err => console.log(err))
+const connectDB = async () => {
+
+  try{
+
+    await mongoose.connect('mongodb://localhost/test', {useNewUrlParser:true, useUnifiedTopology:true, useFindAndModify:false, useCreateIndex:true})
+
+    console.log('Connected Successfully!')
+
+  } 
+  
+  catch(err){
+
+    console.log('Connection Fail!')
+
+  }
+
+}
+
+connectDB()
 
 app.listen(8000, () => console.log('Server Running...'))
+
+exports.default = app
