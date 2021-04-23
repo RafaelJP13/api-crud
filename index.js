17ffc8a3ef2dcf87a097b7481db48460d924df67
Rafael Fernandes <Rafaelsf013@gmail.com>
2021-04-23 02:18:12 -0300

    Adicionar rotas

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express  = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
+const routes = require('./routes/emp')
+
+app.use('/emp', routes)
+
 app.get('/', (req, res) => res.send('Welcome'))
 
 mongoose.connect('mongodb://localhost/teste', {useNewUrlParser:true})
