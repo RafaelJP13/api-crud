38f9c6722b9c2c91e4005b69d3f58547004ef5cb
Rafael Fernandes <Rafaelsf013@gmail.com>
2021-04-24 23:15:27 -0300

    Adicionar funçoes de list, show e delete

--- controller/emp.js
+++ controller/emp.js
@@ -23,6 +23,7 @@ const updateEmp = async (req, res, next) =>{
 
         if(err){
 
+
             res.status(400).json({success:false, msg:'Emp not updated successfully!'})
 
         }
@@ -37,9 +38,78 @@ const updateEmp = async (req, res, next) =>{
 
 }
 
+const readEmps = async (req, res, next) =>{
+
+    await Emp.find( (err, documents) =>{
+
+        if(err){
+
+            res.status(400).json({success:false, msg:'Registers not found!'})
+
+        }
+
+        else{
+
+            res.status(200).json({succes:true, msg: documents})
+            
+        }
+
+    })
+
+}
+
+const readEmp = async (req, res, next) =>{
+
+    const empId = req.params.id
+
+    Emp.findById(empId, (err, documents) =>{
+
+        if(err){
+
+            res.status(400).json({success:false, msg:documents})
+
+        }
+
+        else{
+
+            res.status(200).json({success:true, msg:documents})
+
+        }
+
+    })
+
+}
+
+const deleteEmp = async (req, res, next) =>{
+
+    const empId = req.params.id
+
+    Emp.findByIdAndDelete(empId, (err, documents) => {
+
+        if(err){
+
+            res.status(400).json({success:false, msg:'Emp not deleted successfully'})
+
+        }
+
+        else{
+
+            res.status(200).json({sucess:true, msg:'Emp deleted successfully!'})
+
+
+        }
+
+    })
+
+}
+
+
 module.exports = {
 
     createEmp,
     updateEmp,
+    readEmps,
+    readEmp,
+    deleteEmp,
 
- }
+}
